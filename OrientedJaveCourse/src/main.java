7f123e514bcef086375871d5cecb72f188a4c294
BlackChesire <asafbs177@gmail.com>
2020-09-14 15:41:29 +0300

    comments update

--- OrientedJaveCourse/src/main.java
+++ OrientedJaveCourse/src/main.java
@@ -3,17 +3,17 @@ import java.util.Scanner;
 
 public class main{
 	public static void main(String[] args){
-		int number1,number2,sum;
-		Scanner reader = new Scanner(System.in);
+		int number1,number2,sum; // initialize
+		Scanner reader = new Scanner(System.in); // initialize input
 		System.out.println("enter first number");
-		Random r = new Random();
-		number1 = reader.nextInt();
+		Random r = new Random(); // initialize random generator
+		number1 = reader.nextInt(); // takes client input
 		System.out.println("enter second number");
-		number2 = reader.nextInt();
-		reader.close();
-		sum = number1+number2;
-		int[] numbers = new int[10]; 
-		for(int i =1; i<numbers.length; i++) {
+		number2 = reader.nextInt(); // number2 input
+		reader.close(); // input must be closed!!
+		sum = number1+number2; 
+		int[] numbers = new int[10]; //creating 10 cells dimension 
+		for(int i =1; i<numbers.length; i++) { // for loop from 1 to the matrix length
 			numbers[i] = r.nextInt(100) + 1;
 			System.out.print(numbers[i]);
 		}
@@ -26,7 +26,7 @@ public class main{
 		String name = "asaf";
 		System.out.print(name+"\tthe sum is:");
 		System.out.println(sum);
-		System.out.println("divison is:"+(double)number1/number2); //Convention to double in order to complete
+		System.out.println("divison is:"+(double)number1/number2); //Convention to double 
 		System.out.print(isequal(number2));
 		
 	}
