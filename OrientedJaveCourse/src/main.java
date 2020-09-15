b40692ea3e00a7b0e060c19877d7480f9f0eec40
BlackChesire <asafbs177@gmail.com>
2020-09-15 15:54:49 +0300

    update Polymorphism

--- OrientedJaveCourse/src/main.java
+++ OrientedJaveCourse/src/main.java
@@ -7,5 +7,7 @@ public class main{
 		Car c2 = new Car(50000,7,250,"red","Honda");
 		Car c3 = new Car(7000,6,200,"grey","Toyota");
 		Animal A = new Animal(50,80,"Red");
+		Animal B = new Dog(20,40,"white",6,7,"Husky","Batz",true); // Polymorphism
+		((Dog)B).getType(); // Polymorphism, B is a type of animal , there4 we will use ((dog)) to use methods of dog
 }
 }
