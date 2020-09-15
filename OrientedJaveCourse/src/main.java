e20db3a15f2a625a315ccf9c5a2d062d74058424
BlackChesire <asafbs177@gmail.com>
2020-09-15 14:40:30 +0300

    update

--- OrientedJaveCourse/src/main.java
+++ OrientedJaveCourse/src/main.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class main{
 	public static void main(String[] args){
 		Car c1 = new Car(); //new car instance
-		Car c2 = new Car(50000,7,250,"red","honda");
-		System.out.println(c1.toString());
-		System.out.println(c2.toString());
-	}
+		Car c2 = new Car(50000,7,250,"red","Honda");
+		Car c3 = new Car(7000,6,200,"grey","Toyota");
+		Animal A = new Animal(50,80,"Red");
+}
 }
