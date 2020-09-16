bbf44a77f2fb36ba7fc1136c4d473a861d44807e
Asaf-PC <Asaf-PC@Asaf>
2020-09-17 01:05:35 +0300

    UPdate!

---
+++ OrientedJaveCourse/src/AgeException.java
@@ -0,0 +1,8 @@
+
+public class AgeException extends Exception{
+	
+	private static final long serialVersionUID = 1L;
+	public AgeException(String reason) {
+		super(reason);
+	}
+}
---
+++ OrientedJaveCourse/src/Map.java
@@ -0,0 +1,16 @@
+
+public class Map {
+	private long time;
+	private Tile[][] tiles;
+	public class Tile{
+		private int x;
+		private int y;
+		private int id;
+		public Tile(int x, int y) {
+			this.x = x;
+			this.y = y;
+			id = time / 3 * 5;
+			
+		}
+	}
+}
