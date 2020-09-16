61e6b21c9335f0ec13a502df0723f1b3dc75b99a
Asaf-PC <Asaf-PC@Asaf>
2020-09-17 01:45:00 +0300

    update

--- OrientedJaveCourse/src/AgeException.java
+++ OrientedJaveCourse/src/AgeException.java
@@ -3,6 +3,6 @@ public class AgeException extends Exception{
 	
 	private static final long serialVersionUID = 1L;
 	public AgeException(String reason) {
-		super(reason); // raise!
+		super(reason); // raise
 	}
 }
